f57e13ce10d9d31fa35592e133ee30515e742fa2
Florian Toribio <florian.toribio@etu.unilim.fr>
2021-03-12 10:42:36 +0100

    Initial commit

---
+++ app/src/main/java/com/example/bloc_note_projet/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.bloc_note_projet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
